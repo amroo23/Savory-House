b9903dc87bf7ac6ce44cdb389fc1a4af75064276
amro <amerzendah095@gmail.com>
2026-08-10 20:13:48 +0300

    upgrading the ui

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log("Hello, world!");
+const elements = document.querySelectorAll( ".from-right, .from-left, .from-top, .from-bottom");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+
+    });
+
+}, {
+    threshold: 0.35
+});
+
+elements.forEach((element) => {
+    observer.observe(element);
+});
