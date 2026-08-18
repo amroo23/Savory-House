296f86e6094028750d1285407c83c18fd34afaa9
amro <amerzendah095@gmail.com>
2026-08-18 18:09:45 +0300

    day24

--- js/app.js
+++ js/app.js
@@ -19,3 +19,37 @@ const observer = new IntersectionObserver((entries) => {
 elements.forEach((element) => {
     observer.observe(element);
 });
+
+const form = document.querySelector(".contact-form");
+const message = document.querySelector("#form-message");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const inputs = form.querySelectorAll("input, textarea, select");
+
+    let empty = false;
+
+    inputs.forEach(function (input) {
+        if (input.value.trim() === "") {
+            empty = true;
+        }
+    });
+
+    if (empty) {
+        message.textContent = "Please fill in all fields.";
+        message.classList.remove("success");
+        message.classList.add("error", "show");
+
+    } else {
+        message.textContent = "✓ Your reservation has been sent successfully!";
+        message.classList.remove("error");
+        message.classList.add("success", "show");
+
+        form.reset();
+    }
+
+    setTimeout(function () {
+        message.classList.remove("show");
+    }, 4000);
+});
