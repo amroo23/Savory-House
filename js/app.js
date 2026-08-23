bc84d88195a5ffa5c384567c97186827e2752d51
amro <amerzendah095@gmail.com>
2026-08-23 15:31:43 +0300

    day25

--- js/app.js
+++ js/app.js
@@ -22,9 +22,11 @@ elements.forEach((element) => {
 
 const form = document.querySelector(".contact-form");
 const message = document.querySelector("#form-message");
-
+if (form) {
 form.addEventListener("submit", function (event) {
+    
     event.preventDefault();
+
 
     const inputs = form.querySelectorAll("input, textarea, select");
 
@@ -52,4 +54,74 @@ form.addEventListener("submit", function (event) {
     setTimeout(function () {
         message.classList.remove("show");
     }, 4000);
+});           
+}
+
+
+const contactForm = document.querySelector("#contact-form");
+
+/* =================================== */
+/*           SHOPPING CART LOGIC              */
+/* =================================== */
+
+
+function updateCartCount() {
+  
+    let cart = JSON.parse(localStorage.getItem("savory_cart")) || [];
+    
+    const cartCountElement = document.getElementById("cart-count");
+ 
+    if (cartCountElement) {
+       
+        cartCountElement.textContent = cart.length; 
+    }
+}
+
+updateCartCount();
+
+
+const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
+
+addToCartButtons.forEach(button => {
+    button.addEventListener("click", function() {
+        
+        
+        const itemName = this.getAttribute("data-name");
+        const itemPrice = parseFloat(this.getAttribute("data-price")); 
+
+        let cart = JSON.parse(localStorage.getItem("savory_cart")) || [];
+        
+        cart.push({ name: itemName, price: itemPrice });
+
+    
+        localStorage.setItem("savory_cart", JSON.stringify(cart));
+
+        updateCartCount();
+      
+        const originalText = this.textContent;
+        this.textContent = "✓ Added to Order!";
+        this.classList.add("success"); 
+
+      
+        setTimeout(() => {
+            this.textContent = originalText;
+            this.classList.remove("success");
+        }, 2500);
+        
+    });
 });
+/* ========================================= */
+/*           CART PAGE RENDERING             */
+/* ========================================= */
+
+function renderCartPage() {
+const cartBox = document.getElementById("cart-items-container");
+if(cartBox== null){
+    return
+}
+
+let cart = JSON.parse(localStorage.getItem("savory_cart")) || [];
+
+
+
+}
